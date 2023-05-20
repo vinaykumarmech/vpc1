610488a9cc26df76db2962262bbf0f3897ee6d59
vinaykumarmech <129360296+vinaykumarmech@users.noreply.github.com>
2023-05-20 11:36:07 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -70,10 +70,7 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags {
-    Name        = "${var.name}"
-    Environment = "${var.environment}"
-  }
+ 
 }
 
 /**
